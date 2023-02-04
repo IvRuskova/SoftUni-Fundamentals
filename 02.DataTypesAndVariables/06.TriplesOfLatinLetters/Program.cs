45827f3e18f6f5ad780a8c46faedcc9fb6a8db68
Iva <IvRuskova@gmail.com>
2023-02-04 17:07:19 +0200

    Add TriplesOfLatinLatter, WaterOverFlow and BeerKegs

---
+++ 02.DataTypesAndVariables/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+	for (int j = 0; j < n; j++)
+	{
+		for (int k = 0; k < n; k++)
+		{
+            char firstChar = (char)('a' + i);
+            Console.Write(firstChar);
+
+            char seconrChar = (char)('a' + j);
+            Console.Write(seconrChar);
+
+            char thirdChar = (char)('a' + k);
+			Console.Write(thirdChar);
+
+			Console.WriteLine();
+		}
+	}
+}
---
+++ 02.DataTypesAndVariables/07.WaterOverflow/Program.cs
@@ -0,0 +1,14 @@
+﻿
+byte n = byte.Parse(Console.ReadLine());
+int cntLiters = 0;
+for (int i = 1; i <= n; i++)
+{
+    short liters = short.Parse(Console.ReadLine());
+	cntLiters += (int)liters;
+	if (cntLiters > 255)
+	{
+		Console.WriteLine("Insufficient capacity!");
+		cntLiters -= liters;
+	}	
+}
+Console.WriteLine(cntLiters);

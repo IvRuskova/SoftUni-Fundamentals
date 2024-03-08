08e6ed9e76d745005563c866e3c2740369fc72f6
Iva <IvRuskova@gmail.com>
2024-03-08 23:39:11 +0200

    add lab problems

---
+++ 04.Methods/Lab01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Lab01.SmallestOfThreeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1=int.Parse(Console.ReadLine());
+            int num2=int.Parse(Console.ReadLine());
+            int num3=int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FindSmallest(num1, num2, num3));
+        }
+
+        static int FindSmallest(int num1, int num2, int num3)
+        {
+            if (num1<num2 & num1 < num3)
+            {
+                return num1;     
+            }
+            else if (num2 < num1 & num2 < num3)
+            {
+                return num2;
+            }
+            else 
+            {
+                return num3;
+            }
+            
+        }
+    }
+}
---
+++ 04.Methods/Lab02.VowelsCount/Program.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Lab02.VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string str = Console.ReadLine();
+            Console.WriteLine(FindVowels(str));
+        }
+
+        static int FindVowels(string str)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                switch (str[i])
+                {
+                    case 'A':
+                    case 'a':
+                    case 'O':
+                    case 'o':
+                    case 'U':
+                    case 'u':
+                    case 'E':
+                    case 'e':
+                    case 'I':
+                    case 'i':
+                    case 'Y':
+                    case 'y':
+                        count++;
+                        break;
+                }
+            }
+            return count;
+        }
+    }
+}
